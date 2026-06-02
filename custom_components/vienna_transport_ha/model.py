5c71174ff29f7379152682e7f679dfb0f18f4668
Philipp Slowak <philipp.slowak@gmail.com>
2026-06-02 20:17:25 +0200

    feat: add model

---
+++ custom_components/vienna_transport_ha/model.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass(frozen=True)
+class Vehicle:
+    name: str
+    type: str
+    towards: str
+
+@dataclass(frozen=True)
+class Departure:
+    time_planned: datetime
+    time_real: datetime
+    vehicle: Vehicle
+
+@dataclass(frozen=True)
+class Line:
+    name: str
+    departures: list[Departure]
+
+@dataclass(frozen=True)
+class StopProperties:
+    id: int
+    name: str
+
+@dataclass(frozen=True)
+class Stop:
+    props: StopProperties
+    lines: list[Line]
+
+@dataclass(frozen=True)
+class TransportData:
+    stops: list[Stop]
+    return_code: int
