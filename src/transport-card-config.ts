3096b78211c4909f0a884fde575c2c14014e72e0
Philipp Slowak <philipp.slowak@gmail.com>
2026-09-26 12:56:48 +0200

    fix(card): add prefix to card type

--- src/transport-card-config.ts
+++ src/transport-card-config.ts
@@ -1,7 +1,7 @@
 import type { LovelaceCardConfig } from "./ha.ts";
 
 export interface TransportCardConfig extends LovelaceCardConfig {
-    type: "transport-card";
+    type: "custom:transport-card";
     entity: string;
     lines?: Array<string>;
     max_departures?: number;
